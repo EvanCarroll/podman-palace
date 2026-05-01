70c6626ad94afa57811da7a4f0015b2d2fcfcde1
Evan Carroll <me@evancarroll.com>
2026-04-30 21:06:38 -0500

    fix: map in session bus

--- run-palace.sh
+++ run-palace.sh
@@ -9,11 +9,13 @@ podman run --rm -it \
 	--network=host \
 	-e DISPLAY="$DISPLAY" \
 	-e XAUTHORITY=/tmp/.Xauthority \
+	-e DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" \
+	-v /run/user/$(id -u)/bus:/run/user/$(id -u)/bus \
 	-v "${XAUTHORITY:-$HOME/.Xauthority}":/tmp/.Xauthority:ro \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-	-v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
 	-e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
 	-v /run/user/$(id -u)/pulse/native:/run/user/1000/pulse/native:ro \
+	-v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
 	-e PULSE_COOKIE=/tmp/.pulse-cookie \
 	--userns=keep-id:uid=1000,gid=1000 \
 	-v "$PALACE_DIR":/app/Palace-linux-x64:ro,exec \
