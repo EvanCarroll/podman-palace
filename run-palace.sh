c14103b199e58530367438b312010d2c759fa896
Evan Carroll <me@evancarroll.com>
2026-04-30 21:11:42 -0500

    cleanup

--- run-palace.sh
+++ run-palace.sh
@@ -10,15 +10,15 @@ podman run --rm -it \
 	-e DISPLAY="$DISPLAY" \
 	-e XAUTHORITY=/tmp/.Xauthority \
 	-e DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" \
+	-e PULSE_COOKIE=/tmp/.pulse-cookie \
+	-e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
 	-v /run/user/$(id -u)/bus:/run/user/$(id -u)/bus \
+	-v /run/user/$(id -u)/pulse/native:/run/user/1000/pulse/native:ro \
 	-v "${XAUTHORITY:-$HOME/.Xauthority}":/tmp/.Xauthority:ro \
 	-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-	-e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
-	-v /run/user/$(id -u)/pulse/native:/run/user/1000/pulse/native:ro \
 	-v /run/dbus/system_bus_socket:/run/dbus/system_bus_socket:ro \
-	-e PULSE_COOKIE=/tmp/.pulse-cookie \
-	--userns=keep-id:uid=1000,gid=1000 \
 	-v "$PALACE_DIR":/app/Palace-linux-x64:ro,exec \
+	--userns=keep-id:uid=1000,gid=1000 \
 	--device /dev/dri \
 	--ipc=host \
 	--security-opt label=disable \
